3d3cd2bc1245d1008f78b95fb8264377fa22f216
Madiyar Bekkayrov <madiarbekkajr@gmail.com>
2026-08-19 19:14:21 +0500

    created category and product orm models

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "sqlite:///ecommerce_db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True, pool_size=5, max_overflow=10, pool_pre_ping=True, pool_timeout=30)
 
 SessionLocal = sessionmaker(bind=engine)
 
