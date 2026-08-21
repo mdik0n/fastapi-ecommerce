ceb5eb3440f5b14c2578f03e7fd3aa5c30a2e10e
Madiyar Bekkayrov <madiarbekkajr@gmail.com>
2026-08-21 19:15:32 +0500

    creating session

---
+++ app/db_depends.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from collections.abc import Generator
+
+from app.database import SessionLocal
+
+
+def get_db() -> Generator[Session, None, None]:
+    db: Session = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
