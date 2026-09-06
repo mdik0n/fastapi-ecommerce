dc84be4e810902c78fb17e05af1b3b41aefc3173
Madiyar Bekkayrov <madiarbekkajr@gmail.com>
2026-09-06 21:29:26 +0500

    JWT token creation, user login endpoint

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,20 @@
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.config import SECRET_KEY, ALGORITHM
+from app.db_depends import get_async_db
+from app.models.users import User as UserModel
 
 password_hash = PasswordHash.recommended()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 
 def hash_password(password: str) -> str:
@@ -17,3 +31,65 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     """
 
     return password_hash.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    """
+        Creates JWT token
+    """
+
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: AsyncSession = Depends(get_async_db)
+):
+    """
+        Checks JWT token and returns current user
+    """
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email: str | None = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    user: UserModel | None = (await db.execute(
+        select(UserModel).where(UserModel.email == email, UserModel.is_active == True))).scalar_one_or_none()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
--- app/routers/users.py
+++ app/routers/users.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
+from fastapi.security import OAuth2PasswordRequestForm
+
 from app.db_depends import get_async_db
 from app.schemas import UserCreate, User as UserSchema
 from app.models import User as UserModel
-from app.auth import hash_password, verify_password
+from app.auth import hash_password, verify_password, create_access_token
 
 router = APIRouter(prefix="/users", tags=["users"]
                    )
@@ -31,3 +33,26 @@ async def create_user(user: UserCreate, db: AsyncSession = Depends(get_async_db)
     await db.commit()
 
     return new_user
+
+
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(),
+                db: AsyncSession = Depends(get_async_db)
+                ):
+    """
+    Authenticates user and returns JWT с email, role и id.
+    """
+    email = form_data.username
+    password = form_data.password
+
+    user = (
+        await db.execute(select(UserModel).where(UserModel.email == email, UserModel.is_active))).scalar_one_or_none()
+
+    if user is None or not verify_password(password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user.email, "role": user.role, "id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
